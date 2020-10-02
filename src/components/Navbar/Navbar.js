be9678d72305d019c158380808501ceafab32b32
Adrian Diaz <adrian93eh3@gmail.com>
2020-10-02 12:12:39 -0400

    CSS to the Navbar and the APP overall, will work on other tabs soon

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,39 +1,52 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import "./Navbar.css";
 
 function Navbar() {
     const [click, setClick] = useState(false);
 
     // sets the click above to true by setting it to the opposite value in params
     const handleClick = () => setClick(!click)
-    const closeHamburgerMenu = () => setClick(false);
+
+    // sets the click to false so it turns true upon clicking
+    const closeMobileMenu = () => setClick(false);
 
     return (
         <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to="/">
-                        Home
-                    </Link>
-                    <div className="menu-icon" onClick={handleClick}>
-                        {/* toggles between hamburger and bars for the hamburger menu */}
-                        <i className={click ? "fas fa-times" : "fas fa-bars"} />
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <a href="https://www.linkedin.com/in/adrian-diaz-81044a1aa/" target="_blank" className='navbar-logo' onClick={closeMobileMenu}>
+                        Linkedin
+            <i class="fab fa-linkedin-in"></i>
+                    </a>
+                    <div className='menu-icon' onClick={handleClick}>
+                        {/* Toggles between hamburger and X upon clicking  */}
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
-                    <ul className={click ? "nav-menu active" : "nav-menu"}>
-                        <li className="nav-item">
-                            <Link to="/" className="nav-links" onClick={closeHamburgerMenu}>
+                    {/* Toggles between showing the nav and not showing it  */}
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to="/" className='nav-links' onClick={closeMobileMenu}>
                                 Home
-                            </Link>
+              </Link>
                         </li>
-                        <li className="nav-item">
-                            <Link to="/portfolio" className="nav-links" onClick={closeHamburgerMenu}>
+                        <li className='nav-item'>
+                            <Link
+                                to='/portfolio'
+                                className='nav-links'
+                                onClick={closeMobileMenu}
+                            >
                                 Portfolio
-                            </Link>
+              </Link>
                         </li>
-                        <li className="nav-item">
-                            <Link to="/contact" className="nav-links" onClick={closeHamburgerMenu}>
+                        <li className='nav-item'>
+                            <Link
+                                to='/contact'
+                                className='nav-links'
+                                onClick={closeMobileMenu}
+                            >
                                 Contact Me
-                            </Link>
+              </Link>
                         </li>
                     </ul>
                 </div>
