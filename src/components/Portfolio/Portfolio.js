8d6587b4d6cdfccaa359d7f57d018a334f9ba049
Adrian Diaz <adrian93eh3@gmail.com>
2021-01-25 15:27:39 -0500

    Portfolio page created, and Navbar linked

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
+
+function Portfolio() {
+    return (
+        <div>
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Portfolio;
