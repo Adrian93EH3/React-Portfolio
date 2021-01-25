b16a9888955fecb6a09c877bb6ef7fea9136a64c
Adrian Diaz <adrian93eh3@gmail.com>
2021-01-25 15:20:40 -0500

    commenting out

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
-import Hero from '../Hero/Hero'
+// import Hero from '../Hero/Hero'
 // import Footer from '../Footer/Footer';
 
 function MainPage() {
