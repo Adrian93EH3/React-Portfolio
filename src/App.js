f560a518ad3491644381efed9c434cddbc0f0bfc
Adrian Diaz <adrian93eh3@gmail.com>
2022-07-26 15:00:14 -0400

    removed the importing of the Routes component since it no longer exists

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
-import MyRouter from './components/Routes/Routes';
 
 function App() {
   return (
     <div className="App">
-        <MyRouter />
     </div>
   );
 }
