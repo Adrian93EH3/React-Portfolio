7253c6419ec880d77eaaac5ee018e997b7a006eb
Adrian Diaz <adrian93eh3@gmail.com>
2022-07-26 14:39:45 -0400

    moved image assets into src directory. imported images into mainpage. added a semicolon on project.js

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import Projects from '../ProjectsOnDisplay/Projects';
+import carDealership from '../../assets/images/carDealership.png';
+import weatherApp from '../../assets/images/weatherApp.png';
+import indecisiveCook from '../../assets/images/indecisiveCook.png';
 
 function MainPage() {
     return (
         <div>
             <Navbar />
-            {/* <Hero page="mainPage" /> */}
             <Projects
-                imageSrc={""}
+                imageSrc={carDealership}
                 title={"Car Dealership"}
-                link={"https://emilyleonella.github.io/index.html"}
-                gitHubLink={"https://github.com/Emilyleonella/Emilyleonella.github.io"}
+                link={"https://dry-inlet-97099.herokuapp.com/"}
+                gitHubLink={"https://github.com/Adrian93EH3/CarDealership"}
+            />
+            <Projects
+                imageSrc={weatherApp}
+                title={"Weather App"}
+                link={"https://shielded-shore-16226.herokuapp.com/"}
+                gitHubLink={"https://github.com/Adrian93EH3/Weather-App-React"}
+            />
+            <Projects
+                imageSrc={indecisiveCook}
+                title={"Indecisive Cook"}
+                link={"https://adrian93eh3.github.io/Indecisive-Cook/"}
+                gitHubLink={"https://github.com/Adrian93EH3/Indecisive-Cook"}
             />
         </div>
     )
