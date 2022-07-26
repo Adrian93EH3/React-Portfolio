cd22eb3eea75074576d73acf85c1de0fa975cfcd
Adrian Diaz <adrian93eh3@gmail.com>
2022-07-26 13:35:45 -0400

    removing unnecessary code

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,29 +1,29 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+// import { Link } from 'react-router-dom';
 import "./Navbar.css";
 
 function Navbar() {
-    const [click, setClick] = useState(false);
+    // const [click, setClick] = useState(false);
 
     // sets the click above to true by setting it to the opposite value in params
-    const handleClick = () => setClick(!click)
+    // const handleClick = () => setClick(!click)
 
     // sets the click to false so it turns true upon clicking
-    const closeMobileMenu = () => setClick(false);
+    // const closeMobileMenu = () => setClick(false);
 
     return (
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                    <a href="https://www.linkedin.com/in/adrian-diaz-81044a1aa/" target="_blank" rel="noopener noreferrer" className='navbar-logo' onClick={closeMobileMenu}>
+                    <a href="https://www.linkedin.com/in/adrian-diaz-81044a1aa/" target="_blank" rel="noopener noreferrer" className='navbar-logo'>
                         <i class="fab fa-linkedin-in"></i>
                     </a>
-                    <div className='menu-icon' onClick={handleClick}>
+                    {/* <div className='menu-icon' onClick={handleClick}> */}
                         {/* Toggles between hamburger and X upon clicking  */}
-                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-                    </div>
+                        {/* <i className={click ? 'fas fa-times' : 'fas fa-bars'} /> */}
+                    {/* </div> */}
                     {/* Toggles between showing the nav and not showing it  */}
-                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    {/* <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                         <li className='nav-item'>
                             <Link to="/home" className='nav-links' onClick={closeMobileMenu}>
                                 Home
@@ -34,7 +34,7 @@ function Navbar() {
                                 Portfolio
                              </Link>
                         </li>
-                    </ul>
+                    </ul> */}
                 </div>
             </nav>
         </>
